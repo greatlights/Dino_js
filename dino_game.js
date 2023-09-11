1e8f13784cf4dcd8c0a969b0f794fbe7cd8c0706
greatlights <129233677+greatlights@users.noreply.github.com>
2023-09-11 18:41:56 +0900

    Add files via upload

---
+++ dino_game.js
@@ -0,0 +1,254 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+let score; //현재점수
+let scoreText; //현재 점수 텍스트
+let highscore; //최고 점수
+let highscoreText; //최고 점수 텍스트
+let dino; //공룡
+let gravity; //중력값
+let obstacles = []; //장애물
+let gameSpeed; //게임 속도
+let keys = {}; //키 값
+document.addEventListener("keydown", function (evt) {
+  keys[evt.code] = true;
+});
+document.addEventListener("keyup", function (evt) {
+  keys[evt.code] = false;
+});
+
+class Dino {
+  constructor(x, y, w, h, c) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.c = c;
+
+    this.dy = 0; //점프를 위한
+    this.jumpForce = 15;
+    this.originalHeight = h;
+    this.grounded = false;
+    this.jumpTimer = 0;
+  }
+
+  Draw() {
+    var img = new Image();
+    if ((keys["ShiftLeft"] || keys["KeyS"]) && this.grounded) {
+      img.src = "image/dino_down.png";
+      ctx.drawImage(img, this.x, this.y, this.w, this.h);
+    } else {
+      img.src = "image/dino_up.png";
+      ctx.drawImage(img, this.x, this.y, this.w, this.h);
+    }
+
+    img.src = "image/dino_up.png";
+    ctx.drawImage(img, this.x, this.y, this.w, this.h);
+    // ctx.beginPath();
+    // ctx.fillStyle = this.c;
+    // ctx.fillRect(this.x, this.y, this.w, this.h);
+    // ctx.closePath();
+  }
+  Jump() {
+    if (this.grounded && this.jumpTimer == 0) {
+      this.jumpTimer = 1;
+      this.dy = this.jumpForce;
+    } else if (this.jumpTimer > 0 && this.jumpTimer < 15) {
+      this.jumpTimer++;
+      this.dy = -this.jumpForce - this.jumpTimer / 50;
+    }
+  }
+
+  Animate() {
+    if (keys["Space"] || keys["KeyW"]) {
+      this.Jump();
+    } else {
+      this.jumpTimer = 0;
+    }
+
+    if ((keys["ShiftLeft"] || keys["KeyS"]) && this.grounded) {
+      this.y += this.h / 2;
+      this.h = this.originalHeight / 2;
+    } else {
+      this.h = this.originalHeight;
+    }
+    this.y += this.dy;
+
+    //중력작용
+    if (this.y + this.h < canvas.height) {
+      this.dy += gravity;
+      this.grounded = false;
+    } else {
+      this.dy = 0;
+      this.grounded = true;
+      this.y = canvas.height - this.h;
+    }
+    this.Draw();
+  }
+}
+
+function Start() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  ctx.font = "20px sans-serif";
+
+  gameSpeed = 3;
+  gravity = 1;
+
+  score = 0;
+  highscore = 0;
+
+  if (localStorage.getItem("highscore")) {
+    highscore = localStorage.getItem("highscore");
+  }
+
+  dino = new Dino(25, canvas.height - 150, 50, 50, "pink");
+  scoreText = new Text("Score: " + score, 25, 25, "left", "#212121", "20");
+
+  highscoreText = new Text(
+    "Highscore: " + highscore,
+    canvas.width - 25,
+    25,
+    "right",
+    "#212121",
+    "20"
+  );
+
+  requestAnimationFrame(Update);
+}
+
+let initialSpawnTimer = 200;
+let spawnTimer = initialSpawnTimer;
+
+function Update() {
+  requestAnimationFrame(Update);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  dino.Animate();
+  //   dino.x++;
+
+  spawnTimer--;
+  if (spawnTimer <= 0) {
+    SpawnObstacle();
+    console.log(obstacles);
+    spawnTimer = initialSpawnTimer - gameSpeed * 8;
+
+    if (spawnTimer < 60) {
+      spawnTimer = 60;
+    }
+  }
+
+  for (let i = 0; i < obstacles.length; i++) {
+    let o = obstacles[i];
+
+    if (o.x + o.w < 0) {
+      obstacles.splice(i, 1);
+    }
+    if (
+      dino.x < o.x + o.w &&
+      dino.x + dino.w > o.x &&
+      dino.y < o.y + o.h &&
+      dino.y + dino.h > o.y
+    ) {
+      init();
+    }
+    o.Update();
+  }
+  score++;
+  scoreText.t = "Score: " + score;
+  scoreText.Draw();
+
+  if (score > highscore) {
+    highscore = score;
+    highscoreText.t = "Highscore: " + highscore;
+  }
+
+  highscoreText.Draw();
+
+  gameSpeed += 0.003;
+}
+
+function init() {
+  obstacles = [];
+  score = 0;
+  spawnTimer = initialSpawnTimer;
+  gameSpeed = 3;
+
+  window.localStorage.setItem("highscore", highscore);
+}
+
+class Obstacle {
+  constructor(x, y, w, h, c) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.c = c;
+
+    this.dx = -gameSpeed; //왼쪽으로 달려올 예정
+    this.isBird = false;
+  }
+
+  Update() {
+    this.x += this.dx;
+    this.Draw();
+    this.dx = -gameSpeed;
+  }
+
+  Draw() {
+    var img = new Image();
+    if (this.isBird == true) {
+      img.src = "image/bird.png";
+      ctx.drawImage(img, this.x, this.y, this.w, this.h);
+    } else {
+      img.src = "image/catus.png";
+      ctx.drawImage(img, this.x, this.y, this.w, this.h);
+    }
+    // ctx.beginPath();
+    // ctx.fillStyle = this.c;
+    // ctx.fillRect(this.x, this.y, this.w, this.h);
+    // ctx.closePath();
+  }
+}
+
+function SpawnObstacle() {
+  let size = RandomIntImage(20, 70);
+  let type = RandomIntImage(0, 1);
+  let obstacle = new Obstacle(
+    canvas.width + size,
+    canvas.height - size,
+    size,
+    size,
+    "2484E4"
+  );
+
+  if (type == 1) {
+    obstacle.y -= dino.originalHeight - 10;
+    obstacle.isBird = true;
+  }
+  obstacles.push(obstacle);
+}
+function RandomIntImage(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+class Text {
+  constructor(t, x, y, a, c, s) {
+    this.t = t;
+    this.x = x;
+    this.y = y;
+    this.a = a;
+    this.c = c;
+    this.s = s;
+  }
+
+  Draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.c;
+    ctx.font = this.s + "px sans-serif";
+    ctx.textAlign = this.a;
+    ctx.fillText(this.t, this.x, this.y);
+    ctx.closePath();
+  }
+}
+Start();
